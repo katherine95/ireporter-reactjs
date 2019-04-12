4ee072488cc633f802500b8db83151c80364c964
katherine95 <kathiekim95@gmail.com>
2019-04-12 20:20:29 +0300

    [Feature #165164519]Added signup UI and fetch its endpoint

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,74 +1,78 @@
-import React from 'react';
-import '../App.css';
-import LoginForm from './LoginForm';
+import React from "react";
+import "../App.css";
+import LoginForm from "./LoginForm";
 
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 class Login extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          username: '',
-          password: '',
-          rememberMe: false,
-        };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      rememberMe: false
+    };
 
-    componentDidMount() {
-      console.log('I am already mounted')
-    }
-  
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value})
-    }
-  
-    handleSubmit(event) {
-      event.preventDefault();
-      fetch('https://ireporter-drf-api-staging.herokuapp.com/api/auth/login/', {
-      method: 'POST',
-      headers : {
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("I am already mounted");
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://ireporter-drf-api-staging.herokuapp.com/api/auth/login/", {
+      method: "POST",
+      headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(
-        {
-          username: this.state.username,
-          password: this.state.password
-        },
-        ),})
-        .then(response => response.json())
-        .then(jsondata => {
-          if (jsondata.message === 'Wrong username or password') {
-            document.getElementById('alert').style.color = 'red';
-            document.getElementById('alert').innerHTML = jsondata.message;
-          } else {
-            console.log(jsondata);
-            console.log(jsondata.token);
-            localStorage.setItem('token', jsondata.token);
-            this.setState({
-              data: jsondata,
-            })
-            this.props.history.push('/signup');
-          }
-        });
-    }
-  
-    render() {
-      const {rememberMe, value} = this.state
-      return (
-        <div className="App-header">
-            <div className="container">
-                <div className="left"></div>
-                <div className="right">
-                    <LoginForm rememberMe={rememberMe} value={value} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
-                </div>
-            </div>
-        </div>
-      );
-    }
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+      .then(response => response.json())
+      .then(jsondata => {
+        if (jsondata.message === "Wrong username or password") {
+          document.getElementById("alert").style.color = "red";
+          document.getElementById("alert").innerHTML = jsondata.message;
+        } else {
+          console.log(jsondata);
+          console.log(jsondata.token);
+          localStorage.setItem("token", jsondata.token);
+          this.setState({
+            data: jsondata
+          });
+          this.props.history.push("/redflags");
+        }
+      });
   }
+
+  render() {
+    const { rememberMe, value } = this.state;
+    return (
+      <div className="App-header">
+        <div className="container">
+          <div className="left" />
+          <div className="right">
+            <LoginForm
+              rememberMe={rememberMe}
+              value={value}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default withRouter(Login);
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,40 +1,61 @@
-import React, { Component } from 'react';
-import '../App.css';
-
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+// import "../App.css";
 
 export default class LoginForm extends Component {
-
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
   render() {
-    const {handleChange, handleSubmit, value, rememberMe} = this.props
+    const { handleChange, handleSubmit, value, rememberMe } = this.props;
 
     return (
       <div>
-        <form onSubmit={handleSubmit} className="form">
-        <p id='alert'></p>
-            <label>
-                Username:
-                <input type="text" name="username" required value={value} onChange={handleChange} />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input type="password" name="password" required value={value} onChange={handleChange} />
-            </label>
-            <br />
-            <label>
-                <input name="rememberMe" type="checkbox" checked={rememberMe} onChange={handleChange} />Remember Me
-            </label>
-            <br />
-            <button type="submit" value="Submit" >Log In</button>
-            <br />
-            <br />
-            <p>Create an account?</p><br /><button value="Submit"><a href="/signup">Register</a></button>
-        </form>
+        <p id="alert"></p>
+        <Form onSubmit={handleSubmit} className="form">
+          
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Username"
+              name="username"
+              required
+              value={value}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              required
+              value={value}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicChecbox">
+            <Form.Check
+              type="checkbox"
+              label="Remember me"
+              name="rememberMe"
+              type="checkbox"
+              checked={rememberMe}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" value="submit">
+            Log in
+          </Button>
+          <p>Create an account?</p>
+          <Button value="Submit">
+            <a href="/signup">Register</a>
+          </Button>
+        </Form>
       </div>
-    )
+    );
   }
 }
