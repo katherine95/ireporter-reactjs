360550105ff42819cc234ee07753201a13d5f4b8
katherine95 <kathiekim95@gmail.com>
2019-04-17 13:04:11 +0300

    [Bug]Fix server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express, { static } from 'express';
 var app = express();
-var path = required('path');
+var path = require('path');
 
 app.use(static("dist"));
 app.get('*', (req, res) =>{
-   res.sendFile(path.resolve(__dirname, "/dist", "index.html"));
+   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 app.listen(process.env.PORT || 8080);
